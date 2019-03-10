d8177f5847682f688533db0da26aa2f90a215c16
Pawel Bialas <p.cz.bialas@gmail.com>
2019-03-10 10:40:29 +0100

    get change ot post

--- src/main/java/pl/sdacademy/scrumdemo/ScrumDemo/controller/ContactController.java
+++ src/main/java/pl/sdacademy/scrumdemo/ScrumDemo/controller/ContactController.java
@@ -2,6 +2,7 @@ package pl.sdacademy.scrumdemo.ScrumDemo.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import pl.sdacademy.scrumdemo.ScrumDemo.model.Contact;
@@ -24,16 +25,11 @@ public class ContactController {
 
     }
 
-    @GetMapping(path = "/add")
-    public @ResponseBody String addNewContact (@RequestParam String firstName, @RequestParam String lastName,
-                                               @RequestParam String phoneNumber, @RequestParam String email) {
-        Contact contact = new Contact();
-        contact.setEmail(email);
-        contact.setFirstName(firstName);
-        contact.setLastName(lastName);
-        contact.setPhoneNumber(phoneNumber);
+    @PostMapping(path = "/add")
+    @ResponseStatus(HttpStatus.OK)
+    public void addNewContact (@RequestBody Contact contact) {
         contactService.saveContact(contact);
-        return "Saved!";
+
     }
 
 
